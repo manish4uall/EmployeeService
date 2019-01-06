d63bea3e9393ada3f4f344ff0a1bef26b8a39c44
Manish Verma <mr.manish4uall@gmail.com>
2019-01-06 20:10:11 +0530

    Init

---
+++ EmployeeServiceHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(EmployeeService.EmployeeService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
